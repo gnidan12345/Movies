390ecb6fcee7f8f1b5ebbbc434b5ba564a07b682
Yuliia <gnidan12345@gmail.com>
2022-06-23 00:11:03 +0300

    added object personalMovieDB

---
+++ films.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const numberOfFilms = +prompt("How much films have you watched?",'');
+
+const personalMovieDB = {
+    count : numberOfFilms,
+    movies : {},
+    actor: {},
+    genres: [],
+    private: false
+
+};
+
+const a = prompt("One of the las seen folms?",''),
+      b = prompt("Please, rate the film",''),
+      c = prompt("One of the las seen folms?",''),
+      d = prompt("Please, rate the film",'');
+
+// movies[lastSeenFilm]= 'filmRate';
+
+
+personalMovieDB.movies[a] = b ; 
+personalMovieDB.movies[c] = d ;
+
+console.log(personalMovieDB);
