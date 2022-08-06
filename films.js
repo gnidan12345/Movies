187fe11d6e8af75aa2b0549479b799f91e2989a5
Юлія Гнідан <uliagnidan@MacBook-Air-Ulia.local>
2022-08-06 15:57:25 +0200

    changes on functions

--- films.js
+++ films.js
@@ -1,7 +1,17 @@
 "use strict";
 
-const numberOfFilms = +prompt("How much films have you watched?",'');
+let numberOfFilms;
 
+function start(){
+     numberOfFilms = +prompt("How much films have you watched?",'');
+
+     while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)){
+        numberOfFilms = +prompt("How much films have you watched?",'');
+     }
+
+}
+
+start();
 
 const personalMovieDB = {
     count : numberOfFilms,
@@ -17,28 +27,72 @@ const personalMovieDB = {
 //       c = prompt("One of the las seen folms?",''),
 //       d = prompt("Please, rate the film",'');
 
-for (let i = 0; i < 2 ; i++){
-    const a = prompt("One of the las seen films?",'');
-    const b = prompt("Please, rate the film",'');
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50 ){
-        personalMovieDB.movies[a] = b ; 
-        console.log("done");
-    }else {console.log("error");
-           i--;
+
+
+
+// function rememberMyFilms (){
+//     for (let i = 0; i < 2 ; i++){
+//         const a = prompt("One of the las seen films?",'');
+//         const b = prompt("Please, rate the film",'');
+    
+//         if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50 ){
+//             personalMovieDB.movies[a] = b ; 
+//             console.log("done");
+//         }else {console.log("error");
+//                i--;
+//         }
+//     }
+
+// }
+
+// rememberMyFilms();
+
+
+
+// function detectPersonslLevel(){
+
+//     if (personalMovieDB.count < 10) {
+//         console.log ('Not enough films');
+//     } else if (personalMovieDB.count >= 10  && personalMovieDB.count < 30 ){
+//         console.log ("You are classic viewer")
+//     }else if (personalMovieDB.count >= 30){
+//         console.log ("You are cinoman")
+//     }else{console.log ("Error");
+//     }
+    
+
+// }
+
+// detectPersonslLevel();
+
+
+
+
+
+
+function writeYourGenres (){
+    for(let i=1; i<=3; i++){
+        personalMovieDB.genres[i-1] =  prompt(`Your favorite genre ${i}?`);
+     
     }
 }
+writeYourGenres();
 
-if (personalMovieDB.count < 10) {
-    console.log ('Not enough films');
-} else if (personalMovieDB.count >= 10  && personalMovieDB.count < 30 ){
-    console.log ("You are classic viewer")
-}else if (personalMovieDB.count >= 30){
-    console.log ("You are cinoman")
-}else{console.log ("Error");
+
+function showMyDB(hidden){
+    if (!hidden){
+        console.log(personalMovieDB);
+    }
+    else {
+        console.log('error');
+    }
+
 }
 
+showMyDB(personalMovieDB.private);
 
+// showMyDB(personalMovieDB.private);
     // const a = prompt("One of the las seen films?",'');
     // const b = prompt("Please, rate the film",'');
 
@@ -63,4 +117,4 @@ if (personalMovieDB.count < 10) {
 // personalMovieDB.movies[a] = b ; 
 // personalMovieDB.movies[c] = d ;
 
-console.log(personalMovieDB);
+// console.log(personalMovieDB);
